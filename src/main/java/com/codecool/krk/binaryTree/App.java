7ff26c525a0452690e54e59fe9e71a31b5388cc7
Paprok <rafal.becla@gmail.com>
2019-01-31 20:49:04 +0100

    created class for tests

---
+++ src/main/java/com/codecool/krk/binaryTree/App.java
@@ -0,0 +1,9 @@
+package com.codecool.krk.binaryTree;
+
+public class App{
+
+    public static void main(String[] args) {
+        int[] list = {0,1,2,3,4,5};
+        BinaryTree tree = new BinaryTree(list);
+    }
+}
