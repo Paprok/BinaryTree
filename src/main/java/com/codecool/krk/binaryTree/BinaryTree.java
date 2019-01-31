ab7fdf2e16d52312056f501b92417b17f3460067
Paprok <rafal.becla@gmail.com>
2019-01-31 20:50:09 +0100

    implemented creating binaryTree from list

--- src/main/java/com/codecool/krk/binaryTree/BinaryTree.java
+++ src/main/java/com/codecool/krk/binaryTree/BinaryTree.java
@@ -3,5 +3,8 @@ package com.codecool.krk.binaryTree;
 public class BinaryTree {
     private Node root;
 
-    
+    public BinaryTree(int[] sortedlist) {
+        this.root = new Node(sortedlist, 0, sortedlist.length-1);
+    }
 }
+
--- src/main/java/com/codecool/krk/binaryTree/Node.java
+++ src/main/java/com/codecool/krk/binaryTree/Node.java
@@ -5,9 +5,17 @@ public class Node {
     private Node left;
     private Node right;
 
-    public Node(int value) {
-        this.value = value;
+    public Node(int[] list, int first, int last) {
+        int mid = (last-first)/2 +first;
+        this.value = list[mid];
+        if(mid<last){
+            this.right = new Node(list, mid +1, last);
+        }
+        if(first<mid){
+            this.left = new Node(list, first, mid-1);
+        }
     }
+
 
     public Node getLeft() {
         return left;
